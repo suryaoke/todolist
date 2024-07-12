c9d02a5e17638d657ede298ce77e67af779cec09
suryaoke <89033805+suryaoke@users.noreply.github.com>
2024-07-12 17:23:12 +0700

    oke

--- routes/api.php
+++ routes/api.php
@@ -21,17 +21,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(["midleware" => ["auth:api"]], function () {
 
-    Route::post('checklists/{checklist}/items', [ItemController::class, 'store']);
-    Route::get('checklists/{checklist}/items/{item}', [ItemController::class, 'show']);
-    Route::put('checklists/{checklist}/items/{item}', [ItemController::class, 'update']);
-    Route::put('checklists/{checklist}/items/{item}/toggle', [ItemController::class, 'toggleCompletion']);
-    Route::delete('checklists/{checklist}/items/{item}', [ItemController::class, 'delete']);
-
+    // route checklis //
     Route::get('checklists', [ChecklistController::class, 'index']);
     Route::post('checklists/store', [ChecklistController::class, 'store']);
     Route::delete('checklists/{id}', [ChecklistController::class, 'delete']);
     Route::get('checklists/{id}', [ChecklistController::class, 'show']);
     Route::put('checklists/{id}', [ChecklistController::class, 'update']);
+
+    Route::post('checklists/{checklist}/items', [ItemController::class, 'store']);
+    Route::get('checklists/{checklist}/items/{item}', [ItemController::class, 'show']);
+    Route::put('checklists/{checklist}/items/{item}', [ItemController::class, 'update']);
+    Route::put('checklists/{checklist}/items/{item}/toggle', [ItemController::class, 'toggleCompletion']);
+    Route::delete('checklists/{checklist}/items/{item}', [ItemController::class, 'delete']);
 });
 Route::post('login', [UserController::class, 'login']);
 Route::post('register', [UserController::class, 'register']);
